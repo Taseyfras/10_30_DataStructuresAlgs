016d5b31c369e00d1b3b278cf84c2a3080eff61d
Taseyfras <Taseyfras@gmail.com>
2015-11-02 20:45:04 -0800

    update to my Data Structures and Algs

--- frequency2/frequency2.js
+++ frequency2/frequency2.js
@@ -1,5 +1,31 @@
-var unique = require('./unique.js');
+var unique = require('./unique.js').unique;
+var second = require('./unique.js').second;
+var third = require('./unique.js').third;
+var last = require('./unique.js').last;
 
-var cheeses = ['gouda', 'parmesan', 'brie', 'cheddar', 'bleu'];
+module.exports.frequency = function(array) {
+	var allUniques = [];
+	for(var i =0; i < array.length; i++){
+		allUniques.push(unique(array[i]));
+	} 
+	var superArray = allUniques.reduce(function(a, b){
+		return a.concat(b);
+	});
+	var secondDupesplice = [];
+	secondDupesplice.push(second(superArray));
+	var TheSecond = secondDupesplice.reduce(function(a, b){
+		return a.concat(b);
+	});	
+	var thirdDupesplice = [];
+	thirdDupesplice.push(third(TheSecond));
+	var TheThird = thirdDupesplice.reduce(function(a, b){
+		return a.concat(b);
+	});	
+	var lastDupesplice = [];
+	lastDupesplice.push(last(TheThird));
+	var TheLast = lastDupesplice.reduce(function(a, b){
+		return a.concat(b);
+	});	console.log('The most common letter in your array is ' + TheLast);
+};	
 
-unique.cheeses[1];
+
--- frequency2/unique.js
+++ frequency2/unique.js
@@ -1,18 +1,53 @@
-/*unique: implement a function called unique.
- It takes an array and returns a copy of the array with all the duplicates removed.*/
-
 module.exports.unique = function(string) {
-		for(var i = 0; i < string.length; i++) {
-			for (var j = i + 1; j < string.length; j++) {
-				if (string[i] === string[j]) {
-					string.splice (i, 1);
+	var uniqueArray = string.split('');
+		for(var i = 0; i < uniqueArray.length; i++) {
+			for (var j = i + 1; j < uniqueArray.length; j++) {
+				if (uniqueArray[i] === uniqueArray[j]) {
+					uniqueArray.splice (i, 1);
 				} 
 			}
-		}  return string;
+		}  return uniqueArray;
 	};
+
+module.exports.second = function(array) {
+	var secondUcall = [];
+		for(var i = 0; i < array.length; i++) {
+			for (var j = i + 1; j < array.length; j++) {
+				if (array[i] === array[j]) {
+					secondUcall.push(array.splice (i, 1));
+				} 
+			}
+		}  return secondUcall.reduce(function(a, b){
+		return a.concat(b);
+		});
+	};
+
+module.exports.third = function(array) {
+	var thirdUcall = [];
+		for(var i = 0; i < array.length; i++) {
+			for (var j = i + 1; j < array.length; j++) {
+				if (array[i] === array[j]) {
+					thirdUcall.push(array.splice (i, 1));
+				} 
+			}
+		}  return thirdUcall.reduce(function(a, b){
+		return a.concat(b);
+		});
+	};
+
+module.exports.last = function(array) {
+	var lastUcall = [];
+		for(var i = 0; i < array.length; i++) {
+			for (var j = i + 1; j < array.length; j++) {
+				if (array[i] === array[j]) {
+					lastUcall.push(array.splice (i, 1));
+				} 
+			}
+		}  return lastUcall.reduce(function(a, b){
+		return a.concat(b);
+		});
+	};
+
 
 require('./unique.js');
 
-module.exports.frequency = function(array) {
-	array.forEach()
-};
